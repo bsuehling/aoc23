1f79f0eea49f4eb5a0e388b22ec9bfd8ea673314
Benjamin Sühling <benjamin.suehling@t-online.de>
2023-12-04 09:27:59 +0100

    Day 4

--- day04/main2.py
+++ day04/main2.py
@@ -1,19 +1,23 @@
 def main():
     with open("./day04/input.txt") as f:
-        result = 0
-        for line in f.readlines():
+        lines = list(f.readlines())
+        scratchcards = [1 for _ in lines]
+        for i, line in enumerate(lines):
             line = line[10:]
             wl = line.split("|")
             winning = wl[0].strip(" ").split(" ")
             mine = wl[1].strip(" \n").split(" ")
             winning = list(map(lambda x: int(x), filter(lambda x: x != "", winning)))
             mine = list(map(lambda x: int(x), filter(lambda x: x != "", mine)))
-            points = 0
+
+            correct = 0
             for card in mine:
                 if card in winning:
-                    points = 1 if points == 0 else points * 2
-            result += points
-        print(result)
+                    correct += 1
+            for j in range(i + 1, i + correct + 1):
+                if j < len(scratchcards):
+                    scratchcards[j] += scratchcards[i]
+        print(sum(scratchcards))
 
 
 if __name__ == "__main__":
