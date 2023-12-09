e5580a1fd7ca65ae3c6834c0cf6318a36efbe23f
Benjamin Sühling <benjamin.suehling@t-online.de>
2023-12-09 13:25:40 +0100

    Day 9

---
+++ day09/task1.hs
@@ -0,0 +1,24 @@
+module Main where
+
+stringToInts :: String -> [[Int]]
+stringToInts = map (map read . words) . lines
+
+diffs :: [Int] -> [Int]
+diffs (a:b:t) = (b - a) : diffs (b : t)
+diffs [a]     = []
+diffs []      = []
+
+allZero :: [Int] -> Bool
+allZero = foldr (\h -> (&&) (h == 0)) True
+
+predictValue :: [Int] -> Int
+predictValue x
+  | allZero x = 0
+  | otherwise = last x + predictValue (diffs x)
+
+main :: IO ()
+main = do
+  input <- readFile "./day09/input.txt"
+  let histories = stringToInts input
+  let result = sum . map predictValue $ histories
+  print result
